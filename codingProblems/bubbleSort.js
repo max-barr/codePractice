e9528eacdc8f5f13d7d3783d807486adfbf99308
Max Barr <mxbarr@gmail.com>
2022-07-21 14:54:01 -0700

    Added Bubble Sort algorithm

---
+++ codingProblems/bubbleSort.js
@@ -0,0 +1,16 @@
+let bubbleSort = (arr) => {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+    console.log("Array is now " + arr);
+    return arr;
+};
+
+bubbleSort([23,5,7,6,2,1,45,800,3]);
