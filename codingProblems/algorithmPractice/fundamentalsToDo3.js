abb6e77fdd1acab51b67ba7eb9079fe3b790da11
Max Barr <mxbarr@gmail.com>
2022-07-25 10:35:52 -0700

    Added more problems

--- codingProblems/algorithmPractice/fundamentalsToDo3.js
+++ codingProblems/algorithmPractice/fundamentalsToDo3.js
@@ -111,3 +111,113 @@ evensOdds([2,1,3,2,2,2,1,3,5,2,4,6,1,3,5]);
 
 // *****************************************************************************
 
+// Increment the Seconds
+// Given arr, add 1 to odd elements ([1], [3], etc.), console.log all values and return arr.
+
+const incrementOdds = (arr) => {
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] % 2 != 0) {
+            arr[i]++;
+        }
+    }
+    console.log(arr);
+    return arr;
+}
+incrementOdds([1,2,3,4,5,6,7]);
+
+// *****************************************************************************
+
+// Previous Lengths
+// You are passed an array containing strings. Working within that same array, replace each string with a number – the length of the string at previous array index – and return the array.
+
+const previousLengths = (arr) => {
+    for(let i = 0; i < arr.length; i++) {
+        let stringLength = arr[i].length;
+        arr[i] = stringLength;
+    }
+    console.log(arr);
+    return arr;
+}
+previousLengths(["Hello", "World", "My", "Name", "Is", "String"]);
+
+// *****************************************************************************
+
+// Add Seven to Most
+// Build a function that accepts an array. Return a new array with all values except first, adding 7 to each. Do not alter the original array.
+
+const addSeven = (arr) => {
+    let newArr = [];
+    for(let i = 1; i < arr.length; i++) {
+        newArr.push(arr[i] + 7);
+    }
+    console.log(newArr);
+    return newArr;
+}
+addSeven([3,1,5,7,21]);
+
+// *****************************************************************************
+
+// Reverse Array
+// Given array, write a function to reverse values, in-place. Example: reverse([3,1,6,4,2]) returns same array, containing [2,4,6,1,3].
+
+const reverseArray = (arr) => {
+    arr.reverse();
+    console.log(arr);
+    return arr;    
+}
+reverseArray([3,1,6,4,2]);
+
+// *****************************************************************************
+
+// Outlook: Negative
+// Given an array, create and return a new one containing all the values of the provided array, made negative (not simply multiplied by -1). Given [1,-3,5], return [-1,-3,-5].
+
+const negative = (arr) => {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] > 0) {
+            newArr.push(-Math.abs(arr[i]));
+        } else {
+            newArr.push(arr[i]);
+        }
+    }
+    console.log(newArr);
+    return newArr;
+}
+negative([1,-3,5,-7,7,8,0]);
+
+// *****************************************************************************
+
+// Always Hungry
+// Create a function that accepts an array, and prints "yummy" each time one of the values is equal to "food". If no array elements are "food", then print "I'm hungry" once.
+
+const alwaysHungry = (arr) => {
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === "food") {
+            console.log("yummy");
+        }
+    }
+    if(arr.includes("food") == false) {
+        console.log("I'm hungry");
+    }
+}
+alwaysHungry(["food", 2,3,"food",6,7,8]);
+alwaysHungry([1,2,3,4]);
+
+// *****************************************************************************
+
+// Scale the Array
+// Given array arr and number num, multiply each arr value by num, and return the changed arr.
+
+const scale = (arr, num) => {
+    for(let i =0; i < arr.length; i++) {
+        arr[i] *= num;
+    }
+    console.log(arr);
+    return arr;
+}
+scale([2,4,6,8], 4);
+
+
+
+
