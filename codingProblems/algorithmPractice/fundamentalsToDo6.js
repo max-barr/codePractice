5dc5d1fcdd004b08e07a5d9789e4f89534fe39ba
Max Barr <mxbarr@gmail.com>
2022-08-01 13:26:12 -0700

    More practice problems

--- codingProblems/algorithmPractice/fundamentalsToDo6.js
+++ codingProblems/algorithmPractice/fundamentalsToDo6.js
@@ -64,3 +64,36 @@ const generateCoinChange = (cents) => {
 generateCoinChange(94);
 
 // *****************************************************************************
+
+// Messy Math Mashup
+// Create a function messyMath(num) that will return the following sum: add all integers from 0 up to the given num, except for the following special cases of our count value:
+
+// If current count (not num) is evenly divisible by 3, don’t add to the sum; skip to the next count;
+// Otherwise, if the current count is evenly divisible by 7, include it twice in sum instead of once;
+// Regardless of the above, if the current count is exactly 1/3 of num, return -1 immediately.
+// For example, if given num is 4, return 7. If given num is 8, return 34. If given num is 15, return -1.
+
+const messyMath = (num) => {
+    let count = 0;
+    for (let i = 1; i <= num; i++) {
+        if (num/i == 3) {
+            console.log(-1);
+            return -1;
+        } else if (i % 7 == 0) {
+            count += i*2;
+        } else if (i % 3 == 0) {
+            continue;
+        } else {
+            count += i;
+        }
+    }
+    console.log(count);
+    return count;
+}
+messyMath(4);
+messyMath(3);
+messyMath(8);
+messyMath(15);
+
+// *****************************************************************************
+
